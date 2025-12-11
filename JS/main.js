dd15679d4628ca8761cdf84676cb64c54ae3b23d
teknikaudiovideo6-source <teknikaudiovideo6@gmail.com>
2025-12-11 20:47:08 +0700

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -272,4 +272,28 @@ document.getElementById('saveToExcelBtn')?.addEventListener('click', () => {
       hamburger.classList.remove('active');
     }
   });
+
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1 // 10% of element is visible
+    };
+
+    const elements = document.querySelectorAll('.anggota-card, .kegiatan-card, .section-subtitle');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });
 });
