efa96c8c3a6194ba25ca090dd89260f545ac9375
teknikaudiovideo6-source <teknikaudiovideo6@gmail.com>
2025-12-11 19:24:58 +0700

    Update data.js

--- JS/data.js
+++ JS/data.js
@@ -1,24 +1,24 @@
 // Data Anggota (15 siswa + 3 guru)
 const anggotaData = [
   // Siswa
-  { id: 1, nama: "Adek Arifs,S.S", role: "Bendahara", desc: "perempuan", kategori: ["siswa", "ketua"], img: "img/adek.jpg" },
-  { id: 2, nama: "AHMAD FAREL AFANDI", role: "anggota", desc: "laki", kategori: ["siswa", "ketua"], img: "img/ahmadfarel.jpg" },
-  { id: 3, nama: "ALIA NOVA PRAMITA", role: "Wakil Ketua", desc: "Perempuan", kategori: ["siswa"], img: "img/alianova.jpg" },
+  { id: 1, nama: "Adek Arifs,S.S", role: "Bendahara", desc: "perempuan", kategori: ["siswa", "kesehatan"], img: "img/adek.jpg" },
+  { id: 2, nama: "AHMAD FAREL AFANDI", role: "anggota", desc: "laki", kategori: ["siswa", ], img: "img/ahmadfarel.jpg" },
+  { id: 3, nama: "ALIA NOVA PRAMITA", role: "Wakil Ketua", desc: "Perempuan", kategori: ["siswa","ketua"], img: "img/alianova.jpg" },
   { id: 4, nama: "ANDI ALFAYED TABA", role: "anggota", desc: "laki", kategori: ["siswa"], img: "img/andial.jpg" },
-  { id: 5, nama: "ANDI NURMAN RISQI", role: "Sekertaris", desc: "laki", kategori: ["siswa", "it"], img: "img/andinur.jpg" },
-  { id: 6, nama: "ARZIQ KHOLID SAIFULLAH", role: "anggota", desc: "laki", kategori: ["siswa", "it"], img: "img/arziq.jpg" },
-  { id: 7, nama: "DANISWARA WAHYU WIDYATAMA", role: "anggota", desc: "laki", kategori: ["siswa", "it"], img: "https://via.placeholder.com/220x210/b8e986/000000?text=Raka" },
-  { id: 8, nama: "HABIL QODAM WAHYUD", role: "Ketua Kelas", desc: "laki", kategori: ["siswa", "acara"], img: "img/habil.jpg" },
-  { id: 9, nama: "KEIZYA ADITA ZABRINA", role: "Sekertaris", desc: "permpuan", kategori: ["siswa", "acara"], img: "img/keiza.jpg" },
-  { id: 10, nama: "MISBACH IRSYAD ACMAD", role: "anggota", desc: "laki", kategori: ["siswa", "acara"], img: "img/misba.jpg" },
-  { id: 11, nama: "MOCH. DAMAR AL FAREZT PUTRA K", role: "anggota", desc: "laki", kategori: ["siswa", "kesehatan"], img: "img/damar.jpg" },
-  { id: 12, nama: "MOCH. JABAR ALAM", role: "anggota", desc: "laki", kategori: ["siswa", "kesehatan"], img: "img/jabar.jpg" },
-  { id: 13, nama: "MUHAMMAD FAUZI", role: "anggota", desc: "laki", kategori: ["siswa", "kesehatan"], img: "img/fauzi.jpg" },
+  { id: 5, nama: "ANDI NURMAN RISQI", role: "Sekertaris", desc: "laki", kategori: ["siswa","it" ,"acara"], img: "img/andinur.jpg" },
+  { id: 6, nama: "ARZIQ KHOLID SAIFULLAH", role: "anggota", desc: "laki", kategori: ["siswa", "acara" ], img: "img/arziq.jpg" },
+  { id: 7, nama: "DANISWARA WAHYU WIDYATAMA", role: "anggota", desc: "laki", kategori: ["siswa", ], img: "https://via.placeholder.com/220x210/b8e986/000000?text=Raka" },
+  { id: 8, nama: "HABIL QODAM WAHYUD", role: "Ketua Kelas", desc: "laki", kategori: ["siswa", "ketua","acara"], img: "img/habil.jpg" },
+  { id: 9, nama: "KEIZYA ADITA ZABRINA", role: "Sekertaris", desc: "permpuan", kategori: ["siswa", "it"], img: "img/keiza.jpg" },
+  { id: 10, nama: "MISBACH IRSYAD ACMAD", role: "anggota", desc: "laki", kategori: ["siswa", ], img: "img/misba.jpg" },
+  { id: 11, nama: "MOCH. DAMAR AL FAREZT PUTRA K", role: "anggota", desc: "laki", kategori: ["siswa", "acara"], img: "img/damar.jpg" },
+  { id: 12, nama: "MOCH. JABAR ALAM", role: "anggota", desc: "laki", kategori: ["siswa", "acara" ], img: "img/jabar.jpg" },
+  { id: 13, nama: "MUHAMMAD FAUZI", role: "anggota", desc: "laki", kategori: ["siswa", "acara" ], img: "img/fauzi.jpg" },
   { id: 14, nama: "RIDWAN RAHMAT", role: "anggota", desc: "laki", kategori: ["siswa"], img: "img/ridwan.jpg" },
   { id: 15, nama: "SEPTHA YUGA SONY WIDJAYA", role: "anggota", desc: "laki", kategori: ["siswa"], img: "img/yugha.jpg" },
   
   // Guru
-  { id: 16, nama: "Ismail.", role: "Wali Kelas+Kaprodi", desc: "Guru Bahasa Indonesia & pembimbing karakter", kategori: ["guru"], img: "img/ismail.png" },
+  { id: 16, nama: "Ismail.", role: "Wali Kelas+Kaprodi", desc: "Guru Teknik Jaringan & pembimbing karakter", kategori: ["guru"], img: "img/ismail.png" },
   { id: 17, nama: "Siti Hardiyan.", role: "Guru Bengkel", desc: "Pendamping konseling & pengembangan diri", kategori: ["guru"], img: "img/mahardian.png" },
   { id: 18, nama: "Bagus Catur", role: "Guru Bengkel", desc: "Pembina OSIS & pelatih kepemimpinan", kategori: ["guru"], img: "https://via.placeholder.com/220x210/8e44ad/ffffff?text=Bu+Sari" }
 ];
